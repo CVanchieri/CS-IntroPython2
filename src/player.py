0d29dfad8ceb3d693f7b59fd9e69c64f67054bc7
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-07-14 21:00:12 -0700

    update first P2

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player: # player class
+    def __init__ (self, location): # init method with attribute
+        self.location = location # player attribute
+    def __str__ (self): #str method to use print statement.
+        output = (
+            f'You are in the {self.location} room.'
+            )
+        return (output)
+
+if __name__ == "__main__":
+    player = Player(location='foyer')
+    print(player)
